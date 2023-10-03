d36d0cfaa52914d2cce6b322084e60b566d7679c
Livingstone-Billy <livingstoneochadia@gmail.com>
2023-10-04 00:25:27 +0300

    execute callable entity

--- src/main/java/com/craftinginterpreters/lox/LoxCallable.java
+++ src/main/java/com/craftinginterpreters/lox/LoxCallable.java
@@ -2,6 +2,7 @@ package com.craftinginterpreters.lox;
 
 import java.util.List;
 
+//execute callable entity
 interface LoxCallable {
   int arity();
   Object call(Interpreter interpreter, List<Object> arguments);
