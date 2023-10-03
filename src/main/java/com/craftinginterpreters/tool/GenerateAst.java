4f067453d7875019ecbd789a6a34a3e371a2ed07
Livingstone-Billy <livingstoneochadia@gmail.com>
2023-10-04 00:37:58 +0300

    tool to generate AST

--- src/main/java/com/craftinginterpreters/tool/GenerateAst.java
+++ src/main/java/com/craftinginterpreters/tool/GenerateAst.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
 
+//tool to generate AST
 public class GenerateAst {
   public static void main(String[] args) throws IOException {
     if (args.length != 1) {
